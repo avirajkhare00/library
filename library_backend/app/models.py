5c5a61cb73c3f6fd5facfe1f5ddcf37a709f83c2
avirajkhare00 <avirajkhare00@gmail.com>
2024-01-25 00:17:23 +0530

    [Aviraj] Added models and tests

--- library_backend/app/models.py
+++ library_backend/app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class Shelf(models.Model):
+  space_in_cm = models.IntegerField()
+
+  def __repr__(self):
+    return self.id
+
+
+class Book(models.Model):
+  name = models.CharField(max_length=200)
+  space_in_cm = models.IntegerField()
+  price = models.IntegerField()
+
+  def __repr__(self):
+    return self.name
+
+
+class Catalogue(models.Model):
+  shelf = models.ForeignKey(Shelf, on_delete=models.CASCADE)
+  book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+  def __repr__(self):
+    return str(shelf.id) + '-' + book.name
+
+  @classmethod
+  def total_books(cls, shelf_id):
+    return len(Catalogue.objects.filter(shelf__id=shelf_id))
+
+  @classmethod
+  def total_books_price(cls, shelf_id):
+    total_price = 0
+    for i in Catalogue.objects.filter(shelf__id=shelf_id):
+      total_price += i.book.price
+    return total_price
--- library_backend/app/tests.py
+++ library_backend/app/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
+from .models import Shelf, Book, Catalogue
 
 # Create your tests here.
+
+class ShelfTest(TestCase):
+
+  def test_shelf_space(self):
+    shelf_one = Shelf.objects.create(space_in_cm=10)
+
+    self.assertEqual(shelf_one.space_in_cm, 10)
+
+
+class BookTest(TestCase):
+
+  def test_book_name_and_price(self):
+    book_one = Book.objects.create(name="Jungle Book", space_in_cm="2", price=100)
+    book_two = Book.objects.create(name="Jungle Book Returns", space_in_cm="3", price=150)
+
+    self.assertEqual(book_one.name, "Jungle Book")
+    self.assertEqual(book_two.name, "Jungle Book Returns")
+
+    self.assertEqual(book_one.price, 100)
+    self.assertEqual(book_two.price, 150)
+
+
+class CatalogueTest(TestCase):
+  def test_catalogue_total_books_stored(self):
+    shelf_one = Shelf.objects.create(space_in_cm=10)
+
+    book_one = Book.objects.create(name="Jungle Book", space_in_cm="2", price=100)
+    book_two = Book.objects.create(name="Jungle Book Returns", space_in_cm="3", price=150)
+    book_three = Book.objects.create(name="Gita", space_in_cm="6", price=100)
+
+    catalogue_one = Catalogue.objects.create(shelf=shelf_one, book=book_one)
+    catalogue_two = Catalogue.objects.create(shelf=shelf_one, book=book_two)
+    catalogue_three = Catalogue.objects.create(shelf=shelf_one, book=book_three)
+
+    self.assertEqual(Catalogue.total_books(shelf_one.id), 3)
+
+  def test_catalogue_total_books_price(self):
+    shelf_one = Shelf.objects.create(space_in_cm=10)
+
+    book_one = Book.objects.create(name="Jungle Book", space_in_cm="2", price=100)
+    book_two = Book.objects.create(name="Jungle Book Returns", space_in_cm="3", price=150)
+    book_three = Book.objects.create(name="Gita", space_in_cm="6", price=100)
+
+    catalogue_one = Catalogue.objects.create(shelf=shelf_one, book=book_one)
+    catalogue_two = Catalogue.objects.create(shelf=shelf_one, book=book_two)
+    catalogue_three = Catalogue.objects.create(shelf=shelf_one, book=book_three)
+
+    self.assertEqual(Catalogue.total_books_price(shelf_one.id), 350)
