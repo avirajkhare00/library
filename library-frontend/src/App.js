85597223ded5160f216bcf891a5a242d08e81e81
avirajkhare00 <avirajkhare00@gmail.com>
2024-01-25 13:58:46 +0530

    [Aviraj] Get views of all APIs done

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -7,9 +7,11 @@ import { useEffect, useState } from 'react';
 function App() {
 
   const [books, setBooks] = useState([])
+  const [shelf, setShelf] = useState([])
+  const [catalogue, setCatalogue] = useState([])
 
   useEffect(() => {
-    async function fetchData() {
+    async function fetchBooks() {
       await fetch('http://127.0.0.1:8000/api/v1/books/')
         .then((response) => response.json())
         .then((responseJSON) => {
@@ -17,12 +19,31 @@ function App() {
           setBooks(responseJSON)
         })
     }
-    fetchData()
+    async function fetchShelves() {
+      await fetch('http://127.0.0.1:8000/api/v1/shelves/')
+        .then((response) => response.json())
+        .then((responseJSON) => {
+          console.log(responseJSON)
+          setShelf(responseJSON)
+        })
+    }
+    async function fetchCatalogues() {
+      await fetch('http://127.0.0.1:8000/api/v1/catalogues/')
+        .then((response) => response.json())
+        .then((responseJSON) => {
+          console.log(responseJSON)
+          setCatalogue(responseJSON)
+        })
+    }
+    fetchBooks()
+    fetchShelves()
+    fetchCatalogues()
   }, [])
 
   return (
     <div className="App">
       <h1>Library</h1>
+      <h2>Books</h2>
       {books.map((book) => {
         return (
           <Card style={{ width: '18rem' }}>
@@ -30,6 +51,37 @@ function App() {
               <Card.Title>Name: {book.name}</Card.Title>
               <Card.Text>
                 Price: {book.price}
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        )
+      })}
+      <h2>Shelves</h2>
+      {shelf.map((s) => {
+        return (
+          <Card style={{ width: '18rem' }}>
+            <Card.Body>
+              <Card.Title>ID: {s.id}</Card.Title>
+              <Card.Text>
+                Space(in cm): {s.space_in_cm}
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        )
+      })}
+      <h2>Catalogue</h2>
+      {catalogue.map((c) => {
+        return (
+          <Card style={{ width: '18rem' }}>
+            <Card.Body>
+              <Card.Title>ID: {c.id}</Card.Title>
+              <Card.Text>
+                Book ID: {c.book}
+              </Card.Text>
+              <Card.Text>
+                Shelf ID: {c.shelf}
               </Card.Text>
               <Button variant="primary">Go somewhere</Button>
             </Card.Body>
